d10f2e7d0cb2c430950d79e3535b3b781e4ba5af
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2026-02-26 23:14:35 +0100

    Fixing bugs of deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-    output: 'server',
+    output: "server",
     adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
