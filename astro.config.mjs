e5c400454a3cc024496f2c710a8954c11c3076d6
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2026-02-26 22:52:47 +0100

    Fixing bugs of vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/server';
 
 export default defineConfig({
     output: 'server',
