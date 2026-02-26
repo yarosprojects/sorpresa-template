57b35b9093dd1ef3da274a3e4721b2c6dc3d9866
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2026-02-26 22:48:19 +0100

    Adding serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
     output: 'server',
